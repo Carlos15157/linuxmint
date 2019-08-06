f515a405deb9c2bc42c1a7bf7fa772de5b7716dc
Carlos15157 <53578256+Carlos15157@users.noreply.github.com>
2019-08-06 17:09:14 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -14,3 +14,22 @@ AZUL CLARO SÃO LINKS SIMBOLICOS -- ATALHOS
 BRANCO SÃO OS ARQUIVOS 
 VERDE PERIGO!!!
 sudo apt update && sudo apt instal git = para baixar e instalar o git 
+git clone + url clona o seu arquivo 
+cd linux mint para abrir o diretório 
+echo repete o que você digita 
+echo + (nome) + > + (Nome) = escreve no arquivo
+touch = arquivo
+cat listar conteudo do arquivo 
+cat  LICENSE lê arquivo 
+cat -n LICENSE ve as linhas do arquivo , no caso 674 linhas
+head + (nome) lista as 10 primeiras linhas do arquivo
+head -n20 (nome) lẽ os comandos com a quantidade de linhas que vocẽ quiser
+tail (nome) le as ultimas linhas do arquivo 
+tail -n20 le as 20 ultimas linhas do arquivo 
+less (nome) = paginador , le e pesquisa dentro do arquivo
+cat -n LICENSE | tail = filtrar e ler o final 
+| = executar mais um comando 
+cat -n LICENSE | head -n200 | tail -n101 = listar as linhas desejadas 
+
+
+
