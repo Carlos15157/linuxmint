894426737ef8340e1d683674b4fabe858a0d4d1a
Carlos15157 <53578256+Carlos15157@users.noreply.github.com>
2019-08-06 16:10:44 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,16 @@
 Carlos Henrique de Faria Araújo , SENAC Tatuapé , aprendendo github
+Comandos linux 
+ls  lista os diretórios 
+ls -lh converte tudo em vez de mb
+ctrl + l limpa o terminal 
+cd /  Raiz do Linux
+cd -  volta ao diretório pessol
+pwd mostra o diretório em que você está
+mkdir + (nome) cria mais um diretório
+tab  te ajuda completando os comandos 
+CORES
+AZUL ESCURO SÃO OS DIRETÓRIOS 
+AZUL CLARO SÃO LINKS SIMBOLICOS -- ATALHOS 
+BRANCO SÃO OS ARQUIVOS 
+VERDE PERIGO!!!
+sudo apt update && sudo apt instal git = para baixar e instalar o git 
